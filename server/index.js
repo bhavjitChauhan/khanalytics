9082aa344d3df3da3a8a3c6edf59458a274a929a
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2021-08-25 16:39:11 -0700

    Add `/khan` endpoint proxy

--- server/index.js
+++ server/index.js
@@ -3,13 +3,19 @@ const cors = require('cors');
 const helmet = require("helmet");
 const express = require('express');
 
-require('./jobs/hotlist');
+// require('./jobs/hotlist');
+const khan = require('./routes/khan');
+const hotlist = require('./routes/hotlist');
+
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
 app.use(helmet());
+
+app.use('/hotlist', hotlist);
+app.use('/khan', khan);
 
 app.get('/', (_req, res) => {
     res.send('Hello World!');
---
+++ server/routes/khan.js
@@ -0,0 +1,18 @@
+const { Router } = require('express');
+
+const { get } = require('../lib/api');
+
+
+const router = Router();
+
+router.get('/*', async (req, res) => {
+    try {
+        const data = await get(req.url.slice(1));
+        res.send(data);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+
+module.exports = router;

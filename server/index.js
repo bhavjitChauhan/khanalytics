c0c7176cfc9f4fceadf18bcddb4d6a215acb44aa
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2021-08-25 22:03:50 -0700

    Remove `insertHotlist` job
    In favour of MongoDB Real Triggers.
    https://cloud.mongodb.com/v2/6124257fa9ce3a6f3f7e0e2f#triggers/61271dae6bf167dce117ff11

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const helmet = require("helmet");
 const express = require('express');
 
-// require('./jobs/insertHotlist');
 const khan = require('./routes/khan');
 const hotlist = require('./routes/hotlist');
 

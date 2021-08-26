f4a1088c3745844368d2a7210fc8331d64eca7d9
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2021-08-25 17:21:57 -0700

    Add `find` function

--- server/lib/mongo.js
+++ server/lib/mongo.js
@@ -10,16 +10,44 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
  * 
  * @param {string} name collection name
  * @param {Array.<Object>} docs 
+ * 
+ * @returns {Promise}
  */
 const insertMany = async (name, docs) => {
     const connection = await client.connect();
     const db = await connection.db(process.env.MONGODB_DATABASE);
     const collection = await db.collection(name);
     await collection.insertMany(docs);
+    await client.close();
+};
+
+/**
+ * Alias for `connection.find`.
+ * 
+ * @param {string} name collection name
+ * @param {Object} filter 
+ * 
+ * @returns {Promise<Object>}
+ */
+const find = async (name, filter = {}) => {
+    const connection = await client.connect();
+    const db = await connection.db(process.env.MONGODB_DATABASE);
+    let data;
+    try {
+        const collection = await db.collection(name);
+        const cursor = await collection.find(filter);
+        data = await cursor.toArray();
+        data = data.sort((a, b) => a.timestamp - b.timestamp);
+    } catch (err) {
+        data = err;
+    }
+    await client.close();
+    return data;
 };
 
 
 module.exports = {
     client: client,
-    insertMany: insertMany
+    insertMany: insertMany,
+    find: find
 };

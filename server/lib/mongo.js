d262e49d1c017fcf823e8ce9148aad9332bc5287
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2021-08-27 20:47:29 -0700

    Change MongoDB environment variable

--- server/lib/mongo.js
+++ server/lib/mongo.js
@@ -2,7 +2,7 @@ require('dotenv').config({ path: '../.env' });
 const { MongoClient } = require('mongodb');
 
 
-const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.qj3rg.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`;
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 /**

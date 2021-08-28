13f96548d4907d644bb4ac9339f9f7e7ebf171d8
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2021-08-27 20:46:42 -0700

    Remove `util` directory

--- server/util/sleep.js
+++
@@ -1,9 +0,0 @@
-/**
- * Sleeps for a given duration.
- * 
- * @param {number} ms 
- * @returns {Promise}
- */
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
-
-module.exports = sleep;

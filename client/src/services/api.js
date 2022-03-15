f11dc8297ab8b99ec9d145c3321bd271a9cc8520
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2022-03-14 22:27:21 -0700

    Update API service routes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -12,28 +12,28 @@ export default {
         return response.data;
     },
     async fetchKhanLabs(params) {
-        const response = await instance.get(`khan/labs/${params}`);
+        const response = await instance.get(`/khan/labs/${params}`);
         return response.data;
     },
     async fetchKhanInternal(params) {
-        const response = await instance.get(`khan/internal/${params}`);
+        const response = await instance.get(`/khan/internal/${params}`);
         return response.data;
     },
     async fetchHotlistData(param = 'day') {
-        const response = await instance.get(`hotlist/${param}`);
+        const response = await instance.get(`/hotlist/${param}`);
         return response.data;
     },
     async fetchStatisticsData(param = '') {
-        const response = await instance.get(`statistics/${param}`);
+        const response = await instance.get(`/statistics/${param}`);
         return response.data;
     },
     async fetchPerformanceData(param) {
         if (!param) throw new Error('No param provided');
-        const response = await instance.get(`performance/${param}`);
+        const response = await instance.get(`/performance/${param}`);
         return response.data;
     },
     async postEmail(name, body) {
-        const response = await instance.post(`email`, {
+        const response = await instance.post(`/email`, {
             name,
             body
         });

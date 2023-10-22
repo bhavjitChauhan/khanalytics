e159247aa702c7fcbb7623065887d2cbf3e94283
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2023-10-22 12:13:22 -0700

    Fix heatmap table for `NaN` values

--- client/src/views/Program/HeatMapTable.vue
+++ client/src/views/Program/HeatMapTable.vue
@@ -1,27 +1,18 @@
 <template>
-    <Container
-        :height="height"
-        class="relative overflow-auto"
-    >
-        <table
-            v-if="peaks"
-            class="table w-full table-compact"
-        >
+    <Container :height="height" class="relative overflow-auto">
+        <table v-if="peaks" class="table w-full table-compact">
             <thead>
                 <tr>
                     <th class="text-right">Metric</th>
                     <th>Max</th>
                     <th><a href="https://en.wikipedia.org/wiki/Mean#Arithmetic_mean_(AM)" target="_blank">Mean</a></th>
                     <th><a href="https://en.wikipedia.org/wiki/Median" target="_blank">Median</a></th>
-                    <th><a href="https://en.wikipedia.org/wiki/Standard_deviation" target="_blank"><abbr title="Standard Deviation">S.D.</abbr></a></th>
+                    <th><a href="https://en.wikipedia.org/wiki/Standard_deviation" target="_blank"><abbr
+                                title="Standard Deviation">S.D.</abbr></a></th>
                 </tr>
             </thead>
             <tbody>
-                <tr
-                    v-for="(peak, key) in peaks"
-                    :key="key"
-                    class="text-right hover"
-                >
+                <tr v-for="(peak, key) in peaks" :key="key" class="text-right hover">
                     <th class="text-left">{{ key }}</th>
                     <td>
                         {{ peak.max }}
@@ -101,9 +92,11 @@ export default {
             const peaks = {};
             for (const [key, label] of labels) {
                 const values = performance.map((element) => element[key]);
-                const differences = values.map((value, i, arr) =>
-                    Math.abs(value - (i === 0 ? 0 : arr[i - 1]))
-                );
+                const differences = values
+                    .map((value, i, arr) =>
+                        Math.abs(value - (i === 0 ? 0 : arr[i - 1]))
+                    )
+                    .filter((value) => !isNaN(value));
 
                 peaks[label] = {
                     max: Math.max(...differences),
